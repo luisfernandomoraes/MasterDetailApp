aec9b8ef04a54a5be3826a7d1fb339087133616e
Luís Fernando <luisfernando.pereira.moraes@gmail.com>
2016-04-07 17:52:46 -0300

    Criação das classes de service.

---
+++ MasterApp/MasterApp/View/Services/NavigationService.cs
@@ -0,0 +1,28 @@
+﻿using MasterApp.ViewModels.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MasterApp.Model;
+
+namespace MasterApp.View.Services
+{
+    public class NavigationService : INavigationService
+    {
+        public Task NavigateTo(string viewName, object param)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task NavigateToDetails(Monkey monkey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ShowProperties()
+        {
+            App.Current.MainPage.Navigation.PushAsync()
+        }
+    }
+}
---
+++ MasterApp/MasterApp/ViewModels/Services/INavigationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterApp.ViewModels.Services
+{
+    public interface INavigationService
+    {
+        Task NavigateTo(string viewName, object param);
+        Task ShowProperties();
+        Task NavigateToDetails(Model.Monkey monkey);
+    }
+}
